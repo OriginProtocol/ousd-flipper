b0e00f6c29c4415d2469845973e9ab5c9bd3e735
Daniel Von Fange <daniel@leancoder.com>
2021-02-19 10:49:56 -0500

    Error handling

--- src/SwapBox.svelte
+++ src/SwapBox.svelte
@@ -1,112 +1,125 @@
 <script>
-    import Transaction from "./Transaction.svelte";
-    import {
-        maxFlip,
-        flipperBalances,
-        userBalances,
-        txGasCost,
-    } from "./stores.js";
-    import { writable, derived, get } from "svelte/store";
+  import Transaction from "./Transaction.svelte";
+  import {
+    maxFlip,
+    flipperBalances,
+    userBalances,
+    txGasCost,
+    isConnected,
+  } from "./stores.js";
+  import { writable, derived, get } from "svelte/store";
 
-    export let from;
-    export let to;
-    let notOusd = from != 'OUSD' ? from : to
-    export let flipMethod;
-    export let isOpen = false;
+  export let from;
+  export let to;
+  let notOusd = from != "OUSD" ? from : to;
+  export let flipMethod;
+  export let isOpen = false;
 
-    const balance = flipperBalances[to];
-    $: userBalance = userBalances[from];
-    let available = derived(balance, (b) => {
-        if (b === undefined) {
-            return undefined;
-        }
-        let balance = parseInt(b);
-        if (balance > maxFlip) {
-            return parseInt(maxFlip);
-        }
-        return balance;
-    });
-    let amount = 1000;
-
-    function clickOpen() {
-        isOpen = true;
+  const balance = flipperBalances[to];
+  $: userBalance = userBalances[from];
+  let available = derived(balance, (b) => {
+    if (b === undefined) {
+      return undefined;
     }
-
-    async function afterBuy() {
-        isOpen = false;
-        amount = undefined;
+    let balance = parseInt(b);
+    if (balance > maxFlip) {
+      return parseInt(maxFlip);
     }
+    return balance;
+  });
+  let amount = 1000;
 
+  function clickOpen() {
+    isOpen = true;
+    if ($isConnected) {
+      amount = Math.min(
+        parseInt(get(userBalance)),
+        parseInt(get(balance)),
+        25000
+      );
+    } else {
+      amount = 1000;
+    }
+  }
+
+  async function afterBuy() {
+    isOpen = false;
+    amount = undefined;
+  }
 </script>
 
 <div class="box">
-    <img
-        src="images/{notOusd.toLowerCase()}-radio-on.svg"
-        class="stableIcon"
-        alt={notOusd}
-    />
+  <img
+    src="images/{notOusd.toLowerCase()}-radio-on.svg"
+    class="stableIcon"
+    alt={notOusd}
+  />
 
-    <p style="margin-top:4px">
-        Get <strong>{to}</strong> spending <strong>{from}</strong>
-    </p>
-    <p>
-        {#if $available}
-            <strong>{$available.toLocaleString("en")} {to}</strong> available.
-        {/if}
-        No slippage. {#if $txGasCost}${$txGasCost}{:else}Low{/if} gas.
-    </p>
-
-    {#if parseInt($userBalance) > 1}
-            <p>You have <strong> {parseInt($userBalance).toLocaleString("en")} 
-            {from}</strong>.</p>
-        {:else if parseInt($userBalance) < 20}
-            <p>You don't have enough {from}.</p>
-        {/if}
-
-    {#if !isOpen}
-        <div style="text-align: right;">
-            <button on:click={clickOpen}>Get {to}</button>
-        </div>
-    {:else}
-
-        <table style="margin-left: auto; margin-right: 30px;">
-            <tr>
-                <td>Get</td>
-                <td><input bind:value={amount} /> </td>
-                <td><strong>{to}</strong></td>
-            </tr>
-            <tr>
-                <td>Pay</td>
-                <td><input bind:value={amount} /> </td>
-                <td><strong>{from}</strong></td>
-            </tr>
-        </table>
-        <Transaction
-            {flipMethod}
-            approvalCoin={from}
-            {amount}
-            onComplete={afterBuy}
-        />
+  <p style="margin-top:4px">
+    Get <strong>{to}</strong> spending <strong>{from}</strong>
+  </p>
+  <p>
+    {#if $available}
+      <strong>{$available.toLocaleString("en")} {to}</strong> available.
     {/if}
+    No slippage. {#if $txGasCost}${$txGasCost}{:else}Low{/if} gas.
+  </p>
+
+  {#if parseInt($userBalance) > 1}
+    <p>
+      You have <strong>
+        {parseInt($userBalance).toLocaleString("en")}
+        {from}</strong
+      >.
+    </p>
+  {:else if parseInt($userBalance) < 20}
+    <p>You don't have enough {from}.</p>
+  {/if}
+
+  {#if !isOpen}
+    <div style="text-align: right;">
+      <button on:click={clickOpen}>Get {to}</button>
+    </div>
+  {:else}
+    <table style="margin-left: auto; margin-right: 30px;">
+      <tr>
+        <td>Get</td>
+        <td><input bind:value={amount} /> </td>
+        <td><strong>{to}</strong></td>
+      </tr>
+      <tr>
+        <td>Pay</td>
+        <td><input bind:value={amount} /> </td>
+        <td><strong>{from}</strong></td>
+      </tr>
+    </table>
+    <Transaction
+      {flipMethod}
+      approvalCoin={from}
+      {amount}
+      onComplete={afterBuy}
+      {to}
+    />
+  {/if}
 </div>
 
 <style>
-    .box {
-        padding-bottom: 4px;
-        margin-right: 12px;
-    }
-    p {
-        padding-left: 74px;
-    }
-    hr {
-        border: 1px solid #eee;
-    }
-    .stableIcon {
-        width: 64px;
-        height: 64px;
-        float: left;
-    }
-    input {
-        width: 100px;
-    }
+  .box {
+    padding-bottom: 4px;
+    margin-right: 12px;
+  }
+  p {
+    padding-left: 74px;
+  }
+  hr {
+    border: 1px solid #eee;
+  }
+  .stableIcon {
+    width: 64px;
+    height: 64px;
+    float: left;
+  }
+  input {
+    width: 100px;
+  }
 </style>
--- src/Transaction.svelte
+++ src/Transaction.svelte
@@ -1,77 +1,105 @@
 <script>
-    import {
-        isConnected,
-        connectWallet,
-        setApproval,
-        userAllowances,
-        flip,
-    } from "./stores.js";
+  import { get } from "svelte/store";
 
-    export let approvalCoin;
-    export let flipMethod;
-    export let amount = undefined;
-    export let onComplete = function () {};
+  import {
+    isConnected,
+    connectWallet,
+    setApproval,
+    userAllowances,
+    flipperBalances,
+    userBalances,
+    flip,
+  } from "./stores.js";
 
-    $: allowance = userAllowances[approvalCoin];
-    // $: hasAllowance = $allowance > amount
+  export let approvalCoin;
+  export let flipMethod;
+  export let amount = undefined;
+  export let to = undefined;
+  export let onComplete = function () {};
 
-    if (approvalCoin === undefined) {
-        throw "No Approval stablecoin set";
-    }
+  $: allowance = userAllowances[approvalCoin];
+  let userBalance = userBalances[approvalCoin];
+  let flipperBalance = flipperBalances[to];
 
-    async function approve() {
-        await setApproval(approvalCoin);
-    }
+  $: amountUnderMax = parseInt(amount) <= 25000;
+  $: amountOverMin = parseInt(amount) >= 1;
+  $: amountUnderFlipper = parseInt(amount) <= get(flipperBalance);
+  $: bob = get(flipperBalance);
+  $: anne = parseInt(amount);
+  $: userHas = parseInt(amount) <= parseInt(get(userBalances[approvalCoin]));
 
-    async function buy() {
-        await flip(flipMethod, amount);
-        await onComplete();
-    }
+  console.log($userBalance);
 
-    // if(amount.remove){
+  if (approvalCoin === undefined) {
+    throw "No Approval stablecoin set";
+  }
 
-    // }
+  async function approve() {
+    await setApproval(approvalCoin);
+  }
 
-    // Validate amount
-    // Ensure connect
-    // Ensure allowance
-    // Ensure signature
-    // Ensure submit
-    // Ensure confirm
+  async function buy() {
+    await flip(flipMethod, amount);
+    await onComplete();
+  }
 </script>
 
 <div>
-    <ul>
-        {#if ! $isConnected}
-            <li>1. <button on:click={connectWallet}>🔴 Connect Wallet</button></li>
-        {:else}
-            <li>1. <span class="i">☑︎</span> Connect Wallet - <i>Done</i></li>
+  {#if $isConnected && amountOverMin}
+    {#if !userHas}
+      <p class="error">
+        You don't have <strong>{amount} {approvalCoin}</strong> to trade.
+      </p>
+    {/if}
+    {#if !amountUnderMax}
+      <p class="error">There is a maximum trading amount of $25,000.</p>
+    {/if}
+    {#if !amountUnderFlipper}
+      <p class="error">Swap does not have enough to trade this amount.</p>
+    {/if}
+  {/if}
+  <ul>
+    {#if !$isConnected}
+      <li>
+        1. <button on:click={connectWallet}>🔴 Connect Wallet</button>
+      </li>
+    {:else}
+      <li>1. <span class="i">☑︎</span> Connect Wallet - <i>Done</i></li>
 
-            {#if parseInt($allowance) > parseInt(amount) || parseInt($allowance) > 1}
-                <li>2. <span class="i">☑︎</span> {approvalCoin} Allowance Set - <i>Done</i></li>
-            {:else}
-                <li>
-                    <button on:click={approve}
-                        >2. 🔴 Approve {approvalCoin} Transfer</button>
-                </li>
-            {/if}
-            {#if parseInt($allowance) > parseInt(amount) && $isConnected}
-                <li>3. <button on:click={buy}>Buy!</button></li>
-            {:else}
-                <li>3. Must be connected and have approval before purchase.</li>
-            {/if}
-        {/if}
-    </ul>
+      {#if parseInt($allowance) > parseInt(amount) || parseInt($allowance) > 1}
+        <li>
+          2. <span class="i">☑︎</span>
+          {approvalCoin} Allowance Set - <i>Done</i>
+        </li>
+      {:else}
+        <li>
+          <button on:click={approve}
+            >2. 🔴 Approve {approvalCoin} Transfer</button
+          >
+        </li>
+      {/if}
+      {#if !(parseInt($allowance) > parseInt(amount)) || !$isConnected}
+        <li>3. Must be connected and have approval before purchase.</li>
+      {:else if !amountUnderMax || !amountOverMin || !userHas}
+        <li>3. <button disabled>Swap!</button></li>
+      {:else}
+        <li>3. <button on:click={buy}>Swap!</button></li>
+      {/if}
+    {/if}
+  </ul>
 </div>
 
 <style>
-    li {
-        list-style: none;
-        margin-bottom: 8px;
-    }
-    .i {
-        position: relative;
-        bottom: -2px;
-        font-size: 24px;
-    }
+  li {
+    list-style: none;
+    margin-bottom: 8px;
+  }
+  .i {
+    position: relative;
+    bottom: -2px;
+    font-size: 24px;
+  }
+  .error {
+    color: red;
+  }
 </style>
