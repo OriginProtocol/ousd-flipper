4ff00cb9a619a3e1539e691aadaceeaafe07035b
Daniel Von Fange <daniel@leancoder.com>
2021-02-19 16:47:17 -0500

    Remove days to profit

--- src/Apy.svelte
+++ src/Apy.svelte
@@ -2,18 +2,9 @@
   import { apyData, txGasCost } from "./stores.js";
   import { get, derived } from "svelte/store";
 
-  let daysToProfit = derived([txGasCost, apyData], (d) => {
-    const amount = 5000
-    const [_txGasCost, _apyData] = d
-    if(_txGasCost == undefined || _apyData == undefined){
-      return ""
-    }
-    const breakeven = _txGasCost / (_apyData.apr / 100 * 5000 / 365)
-    return `${parseInt(breakeven)} days to profit on $${amount.toLocaleString("en")} at current gas prices`;
-  });
 </script>
 
-<div style="height: 222px">
+<div style="height: 194px">
   {#if $apyData && $apyData.apy > 0}
     <table style="width:100%">
       <tr>
@@ -33,7 +24,6 @@
       {parseInt($apyData.apy + 0.5)}% APY
     </div>
     <div style="text-align:center">Seven day average</div>
-    <p style="text-align: center;"><i>{$daysToProfit}</i></p>
   {/if}
 </div>
 
